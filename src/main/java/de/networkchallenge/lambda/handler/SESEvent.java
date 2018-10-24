bd6dde0770141a2669eed5f6b08f6134e3131584
Andreas Groll <andreas.groll@gmail.com>
2018-10-24 17:28:24 +0200

    json ignore unknown

--- src/main/java/de/networkchallenge/lambda/handler/SESEvent.java
+++ src/main/java/de/networkchallenge/lambda/handler/SESEvent.java
@@ -1,5 +1,6 @@
 package de.networkchallenge.lambda.handler;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -11,6 +12,7 @@ import java.util.List;
 /**
  * Represents an Amazon SES event.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SESEvent implements Serializable {
 
     /**
